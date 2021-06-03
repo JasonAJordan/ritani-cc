80aba4ce09be510b924d42f6be5c59c3f4e97a80
JasonAkiraJordan <Jasonajordan0@gmail.com>
2021-06-03 00:51:10 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-//import logo from './logo.svg';
 import './App.css';
 
-import React, { useState, useEffect } from "react"; 
+import React, { useState } from "react"; 
 import Search from "./Search"; 
 import UserData from "./UserData";
-import Key from "./keys"; //This is an gitignore file
+import Key from "./keys"; //This file is included in the gitignore file
 
 function App() {
 
